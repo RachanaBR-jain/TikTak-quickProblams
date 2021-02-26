f52bed5ae0bfb6233ca6af0713f90584af51412e
RachanaBR <brrachanajain@gmail.com>
2021-02-26 12:04:00 +0530

    added method choose to take user i/p

--- TikTalToeGame.java
+++ TikTalToeGame.java
@@ -1,23 +1,50 @@
+import java.util.*;
 
 public class TikTalToeGame 
 {
-		public char[] creatingBoard()
+	public char[] creatingBoard()
+	{
+		char[] board= new char[10];
+		for (int  i=1;i<10;i++)
 		{
-			char[] board= new char[10];
-			for (int  i=1;i<=9;i++)
 			{
-				{
-					board[i]=' ';
-				}	
-			}
-			return board;
+				board[i]=' ';
+			}	
 		}
-		
+		return board;
+	}
+	private static char choose(Scanner userInput)
+	{ 
+		System.out.println("enter the user choice letter X or O");
+		return userInput.next().toUpperCase().charAt(0);
+	}   
+	
 	public static void main(String[] args) {
 	TikTalToeGame  game=new TikTalToeGame();
-	game.creatingBoard();
-			
-}
+	System.out.println(game.creatingBoard());
+	Scanner userInput=new Scanner(System.in);
+	char userLetter=choose(userInput);
+	char computerLetter;
+	//computerLatter=(userLetter =='X') ? 'O' : 'Y';
+	if(userLetter=='X')
+	{
+		computerLetter='O';
+	}
+	else if(userLetter=='Y')
+	{
+		computerLetter='Y';
+	}
+	else 
+	{
+		computerLetter='!';
+	}
+	System.out.println(computerLetter);
+	if(computerLetter=='!')
+	{
+		System.out.println("Invalid input plaese give input either O or X");
+	}
+ }
+
 }
 
 
