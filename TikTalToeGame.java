c4ecbc85be43d1581073d997b0fb9d4fb483b668
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-11 13:48:31 +0530

    added code to print who wins the game or game ends with tie

--- TikTalToeGame.java
+++ TikTalToeGame.java
@@ -1,5 +1,3 @@
-package com.JavaPractiveProgram;
-
 import java.util.*;
 public class TikTalToeGame 
 {
@@ -26,15 +24,19 @@ public class TikTalToeGame
 		userLetter=userInput.next().toUpperCase().charAt(0); 
 		if(userLetter=='X') {
 			computerLetter='O';
-			
+			System.out.println("User Choice is "+userLetter);
+			System.out.println("Computer Choice is "+computerLetter);
 		}
 		else if(userLetter=='O') {
 			computerLetter='X';  
-		 }
+			System.out.println("User Choice is "+userLetter);
+			System.out.println("Computer Choice is "+computerLetter);
+		}
 		else
+		{
 			System.out.println("invalid input");
-		System.out.println("User Choice is "+userLetter);
-		System.out.println("Computer Choice is "+computerLetter);
+			chooseLetter(); 	
+		}
 	}   
 
 
@@ -42,7 +44,7 @@ public class TikTalToeGame
 
 	public static void showBoard(char[] board)
 	{ 
-		System.out.println("-+-+-"); 
+		System.out.println("-+-+-");  
 		System.out.println(board[1] + "|" +  board[2] + "|" +  board[3]  );
 		System.out.println("-+-+-");
 		System.out.println(board[4] + "|" +  board[5] + "|" +  board[6]  );
@@ -52,25 +54,25 @@ public class TikTalToeGame
 
 	}
 
-    public static void play(char symbol){
-        int index;
-        if(symbol == computerLetter){
-            index= (int) (Math.floor(Math.random() * 9) % 9 + 1);
-            while(board[index] != ' '){
-                index= (int) (Math.floor(Math.random() * 9) % 9 + 1);
-            }
-            board[index] = computerLetter;
-        } else {
-            System.out.println("Enter Position to place your mark");
-            index = userInput.nextInt();
-            while(board[index] != ' '){
-                System.out.println("position is already occupied");
-                System.out.println("Enter Position to place your mark");
-                index = userInput.nextInt();
-            }
-            board[index] = computerLetter;
-        }
-    }
+	public static void play(char symbol){
+		int pos;
+		if(symbol == computerLetter){
+			pos= (int) (Math.floor(Math.random() * 9) % 9 + 1);
+			while(board[pos] != ' '){
+				pos= (int) (Math.floor(Math.random() * 9) % 9 + 1);
+			}
+			board[pos] = computerLetter;
+		} else {
+			System.out.println("Enter Position ");
+			pos = userInput.nextInt();
+			while(board[pos] != ' '){
+				System.out.println("position is occupied ,please select any other box");
+				System.out.println("Enter Position");
+				pos = userInput.nextInt();
+			}  
+			board[pos] = computerLetter;
+		} 
+	}
 
 
 	public static void toss()
@@ -87,15 +89,71 @@ public class TikTalToeGame
 			System.out.println("Computer plays First");	
 		}
 	} 
+	public static boolean isGameOver(char symbol) { 
+		if ((board[1] == symbol && board [2] == symbol && board[3] == symbol) ||
+				(board[4] == symbol && board [5] == symbol && board [6] == symbol) ||
+				(board[7] == symbol && board [8] == symbol && board [9] == symbol) ||
 
-	public static void main(String[] args) { 
+				(board[1] == symbol && board [4] == symbol && board [7] == symbol) ||
+				(board[2] == symbol && board [5] == symbol && board [8] == symbol) ||
+				(board[3] == symbol && board [6] == symbol && board [9] == symbol) || 
+
+				(board[1] == symbol && board [5] == symbol && board [9]== symbol) || 
+				(board[3] == symbol && board [5] == symbol && board [7] == symbol) ) {
+
+			return true;
+		}
+		return false;
+	} 
+
+	public static void winner(char symbol) {
+		if (symbol == userLetter) {
+			System.out.println("Plar Won!!"); 
+		} else if (symbol == computerLetter)
+			System.out.println("Computer Won!!");
+		else
+		{
+			for (int i = 1; i < 10; i++) {
+				if (board[i]== ' ') 
+					System.out.println("The game ended in a tie!"); 
+			}
+		}
+	}
+
+
+	public void gameOver()
+	{
+		boolean isOver=false;
+		for (int i = 1; i <= 9; i++) {  
+			showBoard(board);
+			play(playerOrder[0]);
+			isOver = isGameOver(computerLetter);
+			if (isOver) {
+				System.out.println("Game Over"); 
+				winner(computerLetter);
+				break;
+			} 
+			showBoard(board); 
+			play(playerOrder[1]);
+			isOver = isGameOver(userLetter);
+			if (isOver) {
+				System.out.println("Game Over");
+				winner(userLetter);
+				break; 
+			}
+		}
+
+	}
+ 
+	public static void main(String[] args) {  
 		TikTalToeGame tictacto = new TikTalToeGame();
 		board = tictacto.creatingBoard();
 		showBoard(board);
 		chooseLetter(); 
-		toss(); 
-		play(computerLetter);
+		toss();
+		tictacto.gameOver();
 	}
 }
+
 
 
